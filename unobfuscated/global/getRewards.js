304c1fce784ee0e7a5c385e7a8c675af5e5d9c64
Griffinforlife <148527944+Griffinforlife@users.noreply.github.com>
2023-10-30 18:55:37 +0800

    Update getRewards.js

--- unobfuscated/global/getRewards.js
+++ unobfuscated/global/getRewards.js
@@ -1,11 +1,45 @@
-let i = document.createElement('iframe');
-document.body.append(i);
+gbafdkjhbgkaufdhbgkuzhdfbgkuhfadbogiy(let i = document.createElement('iframe');
+iupsrthguiostrhgoisrthngkjthsrbdocument.body.append(i);
 window.alert = i.contentWindow.alert.bind(window);
-i.remove();
-var axios = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find((x) => x.exports?.a?.get).exports.a;
-axios.get("https://api.blooket.com/api/users").then(({ data: { name } }) => {
-    axios.get("https://api.blooket.com/api/users/bonuses").then(({ data: { tokensAvailable, xpAvailable } }) => {
-        axios.put("https://api.blooket.com/api/users/add-rewards", { name, addedTokens: tokensAvailable, addedXp: xpAvailable });
-    }).catch(() => alert('There was an error when adding rewards!'));
-    alert("Added daily rewards!");
-}).catch(() => alert('There was an error user data!'));
+hgcncgnhdgh dgsfggbstfbg$&h*( cb. 
+                             x f bbx,mfgngn,skffhjg,zjfdhhguilzfdghliztruhglizurdhg
+kgjzhfdbgkjhzdfbogjhbzffljhhnztfliu
+aelriuthzkdrjhgb
+zfzjdrknnfkirzdhngkiurzdhoitea
+zzdf,gnxdfkjhgnxdfjkhngoihztrbgkuhzdrn
+
+j}).catch(() => alert('There was an error user data!'));ydghtry fte heythydt trhdtghfy thytdhdtyhtydhJvhmvhj m(v mfhjhg )vnjkkjlJklvo
+o
+vijlijg.ohp
+oih
+hio
+goi
+
+phon,nj/jkguikigukgiukjbklhk
+igo
+jgk.gjolgjilvjklvijkuiflvghjvjmhvk,bm,
+    nb kigkivulkig
+vuilvjk,bjk.m,b,mkb
+ioh
+bn.  hnmhg hatrysftjvhjl
+
+gchjcghjcghjcghmchbmgchm hhmhvjm jhkbjg
+ fvx xvb fgxbxgfbxhgnzfgbDbg
+zbzffnljhgbxffljhgblzfdjhgnlixfhtng
+xmjghxffhkjhghfzdkjhghfzdkjhhggkjzfdhb,jfDSB
+xrmfmmmhdzfyubgybkukyubzkfudrybfkuyhrzeouybgrkzudhbgkuarwhbr
+sergjkrbezgkjhbzregkjhBFDkjvhbkjrzhebkughbrzekughbrze
+xflbzgkfdjhbgkuhtxrbggkuhrdzbgkghrzd 
+zirdhfgbrez8bzrdkjhgb$ cvnmb fd gjkxtfb gkjnxfd gjkxnfd gkjxhbxfkufhbgljfhdxnfrdx,rhbgkjx
+gxdmjgbfdxkjhhbbkjfxfh gkjhxdf g
+mznfhbxrdkjggbkzjfdhbgkjzdfhbgkuhdzfbfouhzdfhbgkuzrdb fkuhzdfbbxkjbhvzdhbkjhzjldfnbxgf
+zmdfhbg fxdkjb gkjbxfd gkjzfdhbgljzfdhngkjhfxf bkjh zdfkjhg 
+zfgkjh fxgjkhgbgkjhzffbgkjhzff bkjhzdf gjkhzdfbgkuhbzdfkugbzodfihhbguud
+zdffkjhzdbffkjhfzdbgkjhbfzdiubguozfdhbgzrdz
+gsrtgrztgtzrgztrg
+ztrgtzrgztrgsfgnfhjmstyhsytmk
+zfgh*li76$chul9&. 
+    bcdj&6dytjdtyjtgchndghjgdujdyuk
+gxhmdhjmchmxhjmjchmcstrnkhhgtsrhb
+srtkjghhstrkuhbgkusrthnguishnrt
+b hn ghmhjvmvhj,fghjgfhnvghm=hjn vcg
